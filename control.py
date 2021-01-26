08992928fcd8608298393ca96e39a143be04e3fe
mikityuk <konstantin.mikityuk@psi.ch>
2021-01-26 08:38:37 +0100

    lookup added to control

--- control.py
+++ control.py
@@ -1,4 +1,5 @@
 import sys
+
 #--------------------------------------------------------------------------------------------------
 class Control:
     
@@ -18,34 +19,66 @@ def construct_input():
     inp['t0'] = 0 #default
     inp['t_dt'] = [] #no default
     inp['signal'] = [] #no default
+    inp['lookup'] = [] #no default
 
-    #read input file line by line
+#    #read input file line by line
+#    f = open('input', mode = 'r')
+#    lines = f.readlines()
+#    f.close()
+
+    #read input file as a whole
     f = open('input', mode = 'r')
-    lines = f.readlines()
+    s0 = f.read()
     f.close()
 
-    for line in lines:
-        if line[0] is not '*' :
-            word = line.split()
-            key = word[0].lower()
-            #--------------------------------------------------------------------------------------
-            if key == 't0' :
-                inp['t0'] = float(word[1])
-            #--------------------------------------------------------------------------------------
-            elif key == 't_dt' :
-                inp['t_dt'].append([float(word[1]), float(word[2])])
-            #--------------------------------------------------------------------------------------
-            elif key == 'signal' :
-                 signal = {}
-                 signal['type'] = word[1]
-                 signal['userid'] = word[2]
-                 signal['sign'] = word[3:]
-                 inp['signal'].append(signal)
+    #merge &-ending "line" with the next one
+    s = ''
+    take = True
+    for c in s0 :
+        if c == '&' : take = False
+        if take : s += c
+        if c == '\n' : take = True
 
-    print(inp)
+    #split in lines
+    lines = s.strip().split('\n')
+
+    #remove comment-lines (*)
+    lines = [x for x in lines if not x.startswith('*')]
+
+    def convert_to_float(w) : 
+        try:
+            w = float(w)
+        except :
+            pass
+        return w
+    for line in lines:
+        word = line.split()
+        word = list(map(convert_to_float, word))
+
+        key = word[0].lower()
+        #--------------------------------------------------------------------------------------
+        if key == 't0' :
+            inp['t0'] = word[1]
+        #--------------------------------------------------------------------------------------
+        elif key == 't_dt' :
+            inp['t_dt'].append([word[1], word[2]])
+        #--------------------------------------------------------------------------------------
+        elif key == 'signal' :
+             signal = {}
+             signal['type'] = word[1]
+             signal['userid'] = word[2]
+             signal['sign'] = word[3:]
+             inp['signal'].append(signal)
+        #--------------------------------------------------------------------------------------
+        elif key == 'lookup' :
+             lookup = {}
+             lookup['x'] = word[1::2]
+             lookup['f(x)'] = word[2::2]
+             inp['lookup'].append(lookup)
     #verification
     if inp['t_dt'] == [] :
-        print('ERROR: Card t_dt specifying time_end and dtime_out is obligatory')
+        print('****ERROR: obligatory card t_dt specifying time_end and dtime_out is absent.')
         sys.exit()
 
+    print(inp)
     return inp
