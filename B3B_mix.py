fbcac5824b180bbc0ccfdf4d7c3fc981473330c2
mikityuk <konstantin.mikityuk@psi.ch>
2021-05-01 06:20:48 +0200

    cleaning up of interpolate_temp and interpolate_sig0

--- B3B_mix.py
+++ B3B_mix.py
@@ -31,7 +31,7 @@ class Mix:
     def calculate_sig0(self, indx, core, reactor):
 
         # perform temperature interpolation for all isotopes and return matrix of microscopic XSs without temperature dimension
-        sig_tmp1 = self.interpolate_temp(indx, core, reactor)
+        sig_tmp1 = self.interpolate_temp(core, reactor)
 
         self.sig0 = [[1e10]*self.niso for j in range(self.ng)]
         # if mix consists of only one isotope then keep sig0 = 1e10
@@ -44,7 +44,7 @@ class Mix:
                 while err > 1e-4:
                     # given microscopic XSs without temperature dimension perform sig0 interpolation for energy group ig 
                     # for all isotopes of mix indx and return matrix of microscopic XSs without sig0 dimension
-                    sig_tmp2 = self.interpolate_sig0(ig, indx, core, reactor, sig_tmp1)
+                    sig_tmp2 = self.interpolate_sig0(ig, core, sig_tmp1)
                     err = 0
                     for i in range(self.niso):
                         # find new sig0
@@ -64,12 +64,12 @@ class Mix:
                         sys.exit()
         #print(self.sig0)
     #----------------------------------------------------------------------------------------------
-    # perform temperature interpolation for all isotopes of mix indx and 
+    # perform temperature interpolation for all isotopes of the mix and 
     # return matrix of microscopic XSs without temperature dimension
-    def interpolate_temp(self, indx, core, reactor):
+    def interpolate_temp(self, core, reactor):
 
         # temporal list for sigt after temperature interpolation
-        sig_tmp1 = []
+        sig1 = []
         for i in range(self.niso):
             # index of the isotope i in the global list of isotopes core.iso
             isoindx = [x.isoid for x in core.iso].index(self.isoid[i])
@@ -85,7 +85,7 @@ class Mix:
             if temp < grid_temp[0] or temp > grid_temp[-1]:
                 print('****ERROR: temperature ' + str(temp) + ' K specified in input for isotope ' + self.isoid[i] + ' is out of range of the grid temperatures available in nuclear data library: ' + ''.join([str(int(s)) + ', ' for s in grid_temp])[:-2] + '.')
                 sys.exit()
-            sig_tmp1.append([[0]*nsig0 for j in range(self.ng)])
+            sig1.append([[0]*nsig0 for j in range(self.ng)])
     
             for ig in range(self.ng):
                 for isig0 in range(nsig0):
@@ -93,23 +93,23 @@ class Mix:
                     x = grid_temp
                     y = [core.iso[isoindx].xs['tot'][ig][itemp][isig0] for itemp in range(ntemp)]
                     f = interp1d(x, y) #scipy function
-                    sig_tmp1[i][ig][isig0] = f(temp)
-        return sig_tmp1
+                    sig1[i][ig][isig0] = f(temp)
+        return sig1
 
     #----------------------------------------------------------------------------------------------
-    # given microscopic XSs without temperature dimension perform sig0 interpolation for energy group ig 
-    # for all isotopes of mix indx and return matrix of microscopic XSs without sig0 dimension
-    def interpolate_sig0(self, ig, indx, core, reactor, sig_tmp1):
-        sig_tmp2 = [0]*self.niso
+    # given microscopic XSs without temperature dimension sig1 perform sig0 interpolation for energy group ig 
+    # for all isotopes of the mix and return matrix of microscopic XSs without sig0 dimension
+    def interpolate_sig0(self, ig, core, sig1):
+        sig2 = [0]*self.niso
         for i in range(self.niso):
             # index of the isotope i in the global list of isotopes core.iso
             isoindx = [x.isoid for x in core.iso].index(self.isoid[i])
             # grid sig0s for this isotope
             grid_sig0 = core.iso[isoindx].sig0
             nsig0 = len(grid_sig0)
-            # interpolate total cross section for sig0
-            x = [math.log10(s) for s in grid_sig0]
-            y = [sig_tmp1[i][ig][isig0] for isig0 in range(nsig0)]
+            # interpolate sig1 cross section for sig0
+            x = grid_sig0
+            y = [sig1[i][ig][isig0] for isig0 in range(nsig0)]
             f = interp1d(x, y) #scipy function
-            sig_tmp2[i] = f(math.log10(self.sig0[ig][i]))
-        return sig_tmp2
+            sig2[i] = f(self.sig0[ig][i])
+        return sig2
