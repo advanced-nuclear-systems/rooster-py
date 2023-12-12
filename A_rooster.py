599d6ab5aed56e5baabe0d81268dad439885efae
zhou_l1 <lei.zhou@psi.ch>
2023-12-12 13:35:48 +0100

    Add compile script to package source code to exe.

--- A_rooster.py
+++ A_rooster.py
@@ -1,6 +1,7 @@
 from B_reactor import Reactor
 import time
 import subprocess
+import os
 
 banner = r"""
           +=============================================================+
@@ -18,7 +19,10 @@ copyright = "Copyright | 2020-2023, Advanced Nuclear Systems Group, Paul Scherre
 author    = "Author    | Konstantin Mikityuk, konstantin.mikityuk@psi.ch\n"
 date      = "Date      | " + time.ctime() + "\n"
 try:
-    commit_id = subprocess.check_output(["git","rev-parse","HEAD"]).strip().decode("utf-8")
+    if os.environ.get('ROOSTER_COMMIT_ID') is not None:
+        commit_id = os.environ.get('ROOSTER_COMMIT_ID')
+    else:
+        commit_id = subprocess.check_output(["git","rev-parse","HEAD"]).strip().decode("utf-8")
 except:
     commit_id = "unkown git hash, please install git or run in the repo."
 
